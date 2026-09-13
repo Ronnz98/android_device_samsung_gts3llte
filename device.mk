aa5349209f52e5a987cfad287864eb329ed5376d
Ronnz98 <56398486+Ronnz98@users.noreply.github.com>
2026-09-13 18:15:54 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5 \
+    android.hardware.radio@1.2 \
     android.hardware.radio.config@1.2 \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.2 \
