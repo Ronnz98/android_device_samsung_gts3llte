8839ea8e5c4e724aa25f03b6a4e4a76538c7a3dc
Ronnz98 <56398486+Ronnz98@users.noreply.github.com>
2026-09-25 21:39:12 +0200

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -8,10 +8,10 @@
 
 function blob_fixup() {
     case "${1}" in
-    vendor/lib64/libsec-ril.so)
-        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
-        "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
-        ;;
+        vendor/lib64/libsec-ril.so)
+            "${PATCHELF}" --remove-needed "libhidltransport.so" "${2}"
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+            ;;
     esac
 }
 
